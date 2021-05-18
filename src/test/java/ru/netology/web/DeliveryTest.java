119f39ada6df0582189b9b3d9390755f2ee2be6e
lifanova <veralifanovaa@gmail.com>
2021-05-18 17:28:36 +0300

    Init project

--- src/test/java/ru/netology/web/DeliveryTest.java
+++ src/test/java/ru/netology/web/DeliveryTest.java
@@ -16,7 +16,7 @@ public class DeliveryTest {
         form.$("[data-test-id=phone] input").setValue("+79101234567");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
-        $(".Success_successBlock__2L3Cw").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
     @Test
